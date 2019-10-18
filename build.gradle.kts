066cca3b922fc8398d0a44a09c93ebfea90da24f
Kartoffelsup <kartoffelsup@users.noreply.github.com>
2019-10-19 00:05:17 +0200

    Update to Kotlin 1.3.50
    Signed-off-by: Kartoffelsup <kartoffelsup@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,65 +1,63 @@
-import org.gradle.jvm.tasks.Jar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.jetbrains.kotlin.jvm").version("1.3.20")
+    id("org.jetbrains.kotlin.jvm").version("1.3.50")
 }
 
 group = "com.arml"
 version = "0.0.1-SNAPSHOT"
 
 repositories {
-  mavenLocal()
-  mavenCentral()
-  maven {
-    url = uri("https://dl.bintray.com/kotlin/exposed")
-  }
+    mavenLocal()
+    mavenCentral()
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/exposed")
+    }
 }
 
-val kotlinVersion: String by extra
 val exposedVersion: String by extra
 val mariaDbVersion: String by extra
 val guavaVersion: String by extra
 val argParserVersion: String by extra
 val log4jVersion: String by extra
 
-val jUnitVersion: String  by extra
+val jUnitVersion: String by extra
 val assertJVersion: String by extra
 
 dependencies {
-  compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
-  compile("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-  compile("org.jetbrains.exposed:exposed:$exposedVersion")
-  compile("org.mariadb.jdbc:mariadb-java-client:$mariaDbVersion")
-  compile("com.google.guava:guava:$guavaVersion")
-  compile("com.xenomachina:kotlin-argparser:$argParserVersion")
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    compile("org.jetbrains.kotlin:kotlin-reflect")
+    compile("org.jetbrains.exposed:exposed:$exposedVersion")
+    compile("org.mariadb.jdbc:mariadb-java-client:$mariaDbVersion")
+    compile("com.google.guava:guava:$guavaVersion")
+    compile("com.xenomachina:kotlin-argparser:$argParserVersion")
 
-  compile("org.apache.logging.log4j:log4j-api:$log4jVersion")
-  compile("org.apache.logging.log4j:log4j-core:$log4jVersion")
-  runtime("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
+    compile("org.apache.logging.log4j:log4j-api:$log4jVersion")
+    compile("org.apache.logging.log4j:log4j-core:$log4jVersion")
+    runtime("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
 
-  testCompile("junit:junit:$jUnitVersion")
-  testCompile("org.assertj:assertj-core:$assertJVersion")
+    testCompile("junit:junit:$jUnitVersion")
+    testCompile("org.assertj:assertj-core:$assertJVersion")
 }
 
 tasks.withType<KotlinCompile> {
-  kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 tasks {
-  register("bundle", org.gradle.api.tasks.bundling.Jar::class.java) {
-    dependsOn("build")
-    manifest {
-      attributes["Main-Class"] = "com.arml.realmd.RealmdKt"
+    register("bundle", org.gradle.api.tasks.bundling.Jar::class.java) {
+        dependsOn("build")
+        manifest {
+            attributes["Main-Class"] = "com.arml.realmd.RealmdKt"
+        }
+        archiveBaseName.set("realmd")
+        from(
+            configurations.runtimeClasspath.get().map {
+                if (it.isDirectory)
+                    it
+                else zipTree(it)
+            }
+        )
+        with(getTasks()["jar"] as CopySpec)
     }
-    archiveBaseName.set("realmd")
-    from(
-      configurations.runtimeClasspath.get().map {
-        if (it.isDirectory)
-          it
-        else zipTree(it)
-      }
-    )
-    with(getTasks()["jar"] as CopySpec)
-  }
 }
